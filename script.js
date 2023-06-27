a46903ab92bc81b149e8613ed978a8eef09b75ef
Shivam <73821061+billabonds@users.noreply.github.com>
2023-06-27 21:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ let e = new Promise(resolve => {
 	},5000)
 })
 
-let x = Promise.any([a,b,c,d,e]);
+let x = Promise.any([a, b, c, d, e]);
+window.promises = [a, b, c, d, e];
 
 x.then((data)=>{
 	const element = document.getElementById("output");
